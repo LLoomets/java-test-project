efadbdc7f98c3aaa6de9fbf729d6671766099534
Liisi Loomets <loomets@tlu.ee>
2025-08-18 15:14:08 +0300

    add LogEntry model

---
+++ src/main/java/com/example/java_test_project/model/LogEntry.java
@@ -0,0 +1,25 @@
+package com.example.java_test_project.model;
+
+public class LogEntry {
+    private String page;
+    private int httpCode;
+    private int duration;
+
+    private LogEntry(String page, int httpCode, int duration) {
+        this.page = page;
+        this.httpCode = httpCode;
+        this.duration = duration;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public int getHttpCode() {
+        return httpCode;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
