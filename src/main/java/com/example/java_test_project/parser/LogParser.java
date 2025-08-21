7be8daf5c63045158abc7f060cdd575253c04b75
Liisi Loomets <loomets@tlu.ee>
2025-08-21 16:01:55 +0300

    refactor log parsing by adding log pattern

--- src/main/java/com/example/java_test_project/parser/LogParser.java
+++ src/main/java/com/example/java_test_project/parser/LogParser.java
@@ -4,21 +4,40 @@ package com.example.java_test_project.parser;
 import com.example.java_test_project.model.LogEntry;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class LogParser {
 
+    private static final Pattern logPattern = Pattern.compile(
+            "(\\S+)\\s+" +          // ip
+            "-\\s+-\\s+" +          // dashes
+            "\\[(.*?)]\\s+" +       // timestamp
+            "\"(\\S+)\\s+" +        // http method
+            "(.*?)\"\\s+" +         // page
+            "(\\d{3})\\s+" +        // http code
+            "(\\d+)\\s+" +          // response size
+            "(\\d+)"                // duration
+    );
+
     public LogEntry parseLine(String line) {
         try {
-            String[] parts = line.split("\\s+");
+            Matcher matcher = logPattern.matcher(line);
 
-            String page = parts[6].replace("\"", "");
-            if (page.startsWith("/")) {
-                page = page.substring(1);
+            if (matcher.find()) {
+                String page = matcher.group(4);
+                if (page.startsWith("/")) {
+                    page = page.substring(1);
+                }
+                int httpCode = Integer.parseInt(matcher.group(5));
+                int duration = Integer.parseInt(matcher.group(7));
+
+                return new LogEntry(page, httpCode, duration);
+            } else {
+                System.err.println("log didn't match pattern: " + line);
+                return null;
             }
-            int httpCode = Integer.parseInt(parts[7]);
-            int duration = Integer.parseInt(parts[9]);
-
-            return new LogEntry(page, httpCode, duration);
         } catch (Exception e) {
             System.err.println("failed to parse line: " + line);
             return null;
